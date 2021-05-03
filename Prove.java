e8f0e0215abe4003219914803a23c5d5f626f164
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2021-05-03 21:59:59 +0200

    Update Prove.java

---
+++ Prove.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.io.*;
+import java.util.Base64;
+public class des_1 {
+    public static void main(String[] args){
+    Scanner input=new Scanner(System.in);
+    System.out.print("Shenoni plaintext: ");
+    String plaintext=input.nextLine();
+    }
+    
