b0c8e0638471c4349667e7182dd7f75cee47e8ac
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2021-05-15 16:09:17 +0200

    Update DES.java

--- DES.java
+++ DES.java
@@ -7,8 +7,8 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-import java.util.Scanner
-public class Main {
+import java.util.Scanner;
+public class DES {
         public static void main(String[] args) {
             try {
                 Scanner input = new Scanner(System.in);
@@ -18,6 +18,13 @@ public class Main {
                 SecretKey key = desKeyGenerator.generateKey();
                 desCipherEncrypter.init(Cipher.ENCRYPT_MODE, key);
                 desCipherDecrypter.init(Cipher.DECRYPT_MODE, key);
+
+                System.out.print("File to read: ");
+                String path = input.nextLine();
+                System.out.print("File for encryption: ");
+                String fileenc = input.nextLine();
+                fileencrypt(desCipherEncrypter,path,fileenc);
+               
      
             }
             catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException e ) {
@@ -36,7 +43,24 @@ public class Main {
             return Base64.getEncoder().encodeToString(encryptedBytes);
         }
      
-     
+        public static void fileencrypt(Cipher cipher,String path,String encryptedfile) throws FileNotFoundException,Exception{
+            Scanner inputi=new Scanner(System.in);
+            Scanner finput = null;
+            File file = new File(path);
+            if (!file.exists()) throw new FileNotFoundException();
+            if (!file.canRead()) throw new Exception("Cannot read " + path);
+            if (!file.canWrite()) throw new Exception("Cannot write " + path);
+            finput = new Scanner(file);
+            FileWriter write =new FileWriter(encryptedfile);
+            while (finput.hasNext()) {
+                String str = finput.nextLine();
+                String encrypt = encrypt(cipher, str);
+                write.write(encrypt);
+                write.write("\n");
+            }
+            System.out.println("Encrypted text file");
+            write.close();
+        }
      
      }
 
