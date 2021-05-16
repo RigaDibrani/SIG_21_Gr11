0dcebb6fc823a3747c196b44552b73324f592409
RigaDibrani <74973389+RigaDibrani@users.noreply.github.com>
2021-05-16 19:48:57 +0200

    Update DES.java

--- DES.java
+++ DES.java
@@ -10,7 +10,6 @@ import java.util.Base64;
 import java.util.Scanner;
 public class DES {
 
-
   //creating an instance of the Cipher class for encryption
   private static Cipher encrypt;
   //creating an instance of the Cipher class for decryption
@@ -19,33 +18,34 @@ public class DES {
   private static final byte[] initialization_vector = { 22, 33, 11, 44, 55, 99, 66, 77 };
   public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException,
   IllegalBlockSizeException, InvalidKeyException, InvalidAlgorithmParameterException {
-                //path of the file that we want to encrypt
-                Scanner input=new Scanner(System.in);
-                SecretKey scrtkey = KeyGenerator.getInstance("DES").generateKey();
-                AlgorithmParameterSpec aps = new IvParameterSpec(initialization_vector);
-                encrypt=Cipher.getInstance("DES/CBC/PKCS5Padding");
-                decrypt= Cipher.getInstance("DES/CBC/PKCS5Padding");
-                
+                 //path of the file that we want to encrypt
+                 Scanner input=new Scanner(System.in);
+                 SecretKey scrtkey = KeyGenerator.getInstance("DES").generateKey();
+                 AlgorithmParameterSpec aps = new IvParameterSpec(initialization_vector);
+                 encrypt=Cipher.getInstance("DES/CBC/PKCS5Padding");
+                 decrypt= Cipher.getInstance("DES/CBC/PKCS5Padding");
               
-                // Initialize the cipher for encryption and decryption
-                encrypt.init(Cipher.ENCRYPT_MODE, scrtkey,aps);
-                decrypt.init(Cipher.DECRYPT_MODE, scrtkey,aps);
+                 // Initialize the cipher for encryption and decryption
+                 encrypt.init(Cipher.ENCRYPT_MODE, scrtkey,aps);
+                 decrypt.init(Cipher.DECRYPT_MODE, scrtkey,aps);
 
                  //----------------------------------------TEXT/FILE ENCRYPTION/DECRYPTION----------------------------------------------//
-                  System.out.print("Zgjedhni text ose file : ");
-                  String fromuser=user.nextLine();
-                  fromuser=fromuser.toLowerCase();
-                  if(fromuser.equals("text")){
-                      text();
-                  }
-                  else if(fromuser.equals("file")){
-                      file();
-                  }
-                  else
-                      System.out.print("\nGabim!!!");
+                 System.out.print("Zgjedhni text ose file : ");
+                 String fromuser=user.nextLine();
+                 fromuser=fromuser.toLowerCase();
+                 if(fromuser.equals("text")){
+                     text();
+                 }
+                 else if(fromuser.equals("file")){
+                     file();
+                 }
+                 else
+                     System.out.print("\nGabim!!!");
               
                }
-         public static void text() throws BadPaddingException, IllegalBlockSizeException {
+  
+  //----------------------------------------TEXT/FILE ENCRYPTION/DECRYPTION-----------------------------------------//
+           public static void text() throws BadPaddingException, IllegalBlockSizeException {
 
                 Scanner input=new Scanner(System.in);
                 System.out.print("Text:");
@@ -57,46 +57,79 @@ public class DES {
                 String decrypted = decrypt(decrypt, encrypted);
                 System.out.println("Decrypted text: " + decrypted);
                 input.close();
-            
           }
+  
+           public static void file() throws IOException {
+                Scanner input=new Scanner(System.in);
+                System.out.println("File to encrypt:");
+                String textFile=input.nextLine();
 
+                System.out.println("Where to save encrypted file:");
+                String encryptedData =input.nextLine();
+
+                System.out.println("Where to save decrypted file:");
+                String decryptedData = input.nextLine();
+
+                //calling encrypt() method to encrypt the file
+                encryption(new FileInputStream(textFile), new FileOutputStream(encryptedData));
+                //calling decrypt() method to decrypt the file
+                decryption(new FileInputStream(encryptedData), new FileOutputStream(decryptedData));
+                //prints the statement if the program runs successfully
+                System.out.println("The encrypted and decrypted files have been created successfully.");
+                input.close();
+          }
+  
           //----------------------------------------TEXT ENCRYPTION/DECRYPTION----------------------------------------------//
-          public static String encrypt(Cipher cipher, String plainStr) throws BadPaddingException,IllegalBlockSizeException
+           public static String encrypt(Cipher cipher, String plainStr) throws BadPaddingException,IllegalBlockSizeException
           {
-              byte[] plainBytes;
-              byte[] encryptedBytes;
-              plainBytes=plainStr.getBytes(StandardCharsets.UTF_8);
-              encryptedBytes=cipher.doFinal(plainBytes);
-              //return new String(encryptedBytes,StandardCharsets.ISO_8859_1);
-              return Base64.getEncoder().encodeToString(encryptedBytes);
+                byte[] plainBytes;
+                byte[] encryptedBytes;
+                plainBytes=plainStr.getBytes(StandardCharsets.UTF_8);
+                encryptedBytes=cipher.doFinal(plainBytes);
+                //return new String(encryptedBytes,StandardCharsets.ISO_8859_1);
+                return Base64.getEncoder().encodeToString(encryptedBytes);
           }
 
-          public static String decrypt(Cipher cipher,String encryptedStr) throws BadPaddingException,IllegalBlockSizeException
+           public static String decrypt(Cipher cipher,String encryptedStr) throws BadPaddingException,IllegalBlockSizeException
           {
-              byte[] encryptedBytes=Base64.getDecoder().decode(encryptedStr);
-              //byte[] encryptedBytes=encryptedStr.getBytes(StandardCharsets.ISO_8859_1);
-              byte[] decryptedBytes=cipher.doFinal(encryptedBytes);
-              return new String(decryptedBytes,StandardCharsets.UTF_8);
+                byte[] encryptedBytes=Base64.getDecoder().decode(encryptedStr);
+                //byte[] encryptedBytes=encryptedStr.getBytes(StandardCharsets.ISO_8859_1);
+                byte[] decryptedBytes=cipher.doFinal(encryptedBytes);
+                return new String(decryptedBytes,StandardCharsets.UTF_8);
           }
+  
            //----------------------------------------FILE ENCRYPTION/DECRYPTION----------------------------------------------//
-          private static void encryption(InputStream input, OutputStream output)
-          throws IOException
+           private static void encryption(InputStream input, OutputStream output)
+           throws IOException
           {
-              output = new CipherOutputStream(output, encrypt);
-              //calling the writeBytes() method to write the encrypted bytes to the file
-              writeBytes(input, output);
+                output = new CipherOutputStream(output, encrypt);
+                //calling the writeBytes() method to write the encrypted bytes to the file
+                writeBytes(input, output);
           }
-          //method for decryption
-          private static void decryption(InputStream input, OutputStream output)
-                  throws IOException
+  
+           //method for decryption
+           private static void decryption(InputStream input, OutputStream output)
+           throws IOException
           {
-              input = new CipherInputStream(input, decrypt);
-              //calling the writeBytes() method to write the decrypted bytes to the file
-              writeBytes(input, output);
+                input = new CipherInputStream(input, decrypt);
+                //calling the writeBytes() method to write the decrypted bytes to the file
+                writeBytes(input, output);
           }
             
+           //method for writting bytes to the files
+           private static void writeBytes(InputStream input, OutputStream output)
+           throws IOException
+          {
+                byte[] writeBuffer = new byte[512];
+                int readBytes;
+                while ((readBytes = input.read(writeBuffer)) >= 0)
+                {
+                    output.write(writeBuffer, 0, readBytes);
+                }
 
-
-            
+                 //closing the output stream
+                 output.close();
+                 //closing the input stream
+                 input.close();
+          }      
      }
-
