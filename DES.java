3188dd7b1903b8b151b9070ee3b5ca054ad53f3a
rinajasharaj <75223541+rinajasharaj@users.noreply.github.com>
2021-05-16 18:36:34 +0200

    Update DES.java

--- DES.java
+++ DES.java
@@ -9,23 +9,38 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Scanner;
 public class DES {
-        public static void main(String[] args) {
-            try {
-                Scanner input = new Scanner(System.in);
-                KeyGenerator desKeyGenerator = KeyGenerator.getInstance("DES");
-                Cipher desCipherEncrypter = Cipher.getInstance("DES");
-     
-                SecretKey key = desKeyGenerator.generateKey();
-                desCipherEncrypter.init(Cipher.ENCRYPT_MODE, key);
-                desCipherDecrypter.init(Cipher.DECRYPT_MODE, key);
+
+
+  //creating an instance of the Cipher class for encryption
+  private static Cipher encrypt;
+  //creating an instance of the Cipher class for decryption
+  private static Cipher decrypt;
+  //initializing vector
+  private static final byte[] initialization_vector = { 22, 33, 11, 44, 55, 99, 66, 77 };
+  public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException,
+  IllegalBlockSizeException, InvalidKeyException, InvalidAlgorithmParameterException {
+                //path of the file that we want to encrypt
+                Scanner input=new Scanner(System.in);
+                SecretKey scrtkey = KeyGenerator.getInstance("DES").generateKey();
+                AlgorithmParameterSpec aps = new IvParameterSpec(initialization_vector);
+                encrypt=Cipher.getInstance("DES/CBC/PKCS5Padding");
+                decrypt= Cipher.getInstance("DES/CBC/PKCS5Padding");
                 
-                System.out.print("Enter plaintextin: ");
-                String message = input.nextLine();
-                String encrypted = encrypt(desCipherEncrypter, message);
+                //----------------------------------------TEXT ENCRYPTION/DECRYPTION----------------------------------------------//
+
+                // Initialize the cipher for encryption and decryption
+                encrypt.init(Cipher.ENCRYPT_MODE, scrtkey,aps);
+                decrypt.init(Cipher.DECRYPT_MODE, scrtkey,aps);
+
+                System.out.print("Text:");
+                String plain=input.nextLine();
+
+                String encrypted = encrypt(encrypt, plain);
                 System.out.println("Encrypted text: " + encrypted);
-                String decrypted = decrypt(desCipherDecrypter, encrypted);
+
+                String decrypted = decrypt(decrypt, encrypted);
                 System.out.println("Decrypted text: " + decrypted);
-                
+
                 //file
                 System.out.print("File to read: ");
                 String path = input.nextLine();
@@ -57,14 +72,24 @@ public class DES {
         }
      
 
-        public static String encrypt(Cipher cipher, String plainStr) throws BadPaddingException,IllegalBlockSizeException
-        {
-            byte[] plainBytes;
-            byte[] encryptedBytes;
-            plainBytes=plainStr.getBytes(StandardCharsets.ISO_8859_1);
-            encryptedBytes=cipher.doFinal(plainBytes);
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        }
+          //----------------------------------------TEXT ENCRYPTION/DECRYPTION----------------------------------------------//
+          public static String encrypt(Cipher cipher, String plainStr) throws BadPaddingException,IllegalBlockSizeException
+          {
+              byte[] plainBytes;
+              byte[] encryptedBytes;
+              plainBytes=plainStr.getBytes(StandardCharsets.UTF_8);
+              encryptedBytes=cipher.doFinal(plainBytes);
+              //return new String(encryptedBytes,StandardCharsets.ISO_8859_1);
+              return Base64.getEncoder().encodeToString(encryptedBytes);
+          }
+
+          public static String decrypt(Cipher cipher,String encryptedStr) throws BadPaddingException,IllegalBlockSizeException
+          {
+              byte[] encryptedBytes=Base64.getDecoder().decode(encryptedStr);
+              //byte[] encryptedBytes=encryptedStr.getBytes(StandardCharsets.ISO_8859_1);
+              byte[] decryptedBytes=cipher.doFinal(encryptedBytes);
+              return new String(decryptedBytes,StandardCharsets.UTF_8);
+          }
      
         public static void fileencrypt(Cipher cipher,String path,String encryptedfile) throws FileNotFoundException,Exception{
             Scanner inputi=new Scanner(System.in);
@@ -99,12 +124,7 @@ public class DES {
             System.out.println("Decrypted text");
     }
             
-            public static String decrypt(Cipher cipher,String encryptedStr) throws BadPaddingException,IllegalBlockSizeException
-    {
-            byte[] encryptedBytes=Base64.getMimeDecoder().decode(encryptedStr);
-            byte[] decryptedBytes=cipher.doFinal(encryptedBytes);
-            return new String(decryptedBytes,StandardCharsets.UTF_8);
-    }
+
 
             
      }
