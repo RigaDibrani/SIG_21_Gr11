ab6cfe8a22273fc10c59f701899507931f9ac9d5
valzagrainca <75394963+valzagrainca@users.noreply.github.com>
2021-05-16 19:34:48 +0200

    Update DES.java
    Zgjedh nese deshiron te enkriptoj/dekriptoj file apo text,dhe enkriptim/dekriptimi i fajllav

--- DES.java
+++ DES.java
@@ -26,51 +26,39 @@ public class DES {
                 encrypt=Cipher.getInstance("DES/CBC/PKCS5Padding");
                 decrypt= Cipher.getInstance("DES/CBC/PKCS5Padding");
                 
-                //----------------------------------------TEXT ENCRYPTION/DECRYPTION----------------------------------------------//
-
+              
                 // Initialize the cipher for encryption and decryption
                 encrypt.init(Cipher.ENCRYPT_MODE, scrtkey,aps);
                 decrypt.init(Cipher.DECRYPT_MODE, scrtkey,aps);
 
+                 //----------------------------------------TEXT/FILE ENCRYPTION/DECRYPTION----------------------------------------------//
+                  System.out.print("Zgjedhni text ose file : ");
+                  String fromuser=user.nextLine();
+                  fromuser=fromuser.toLowerCase();
+                  if(fromuser.equals("text")){
+                      text();
+                  }
+                  else if(fromuser.equals("file")){
+                      file();
+                  }
+                  else
+                      System.out.print("\nGabim!!!");
+              
+               }
+         public static void text() throws BadPaddingException, IllegalBlockSizeException {
+
+                Scanner input=new Scanner(System.in);
                 System.out.print("Text:");
                 String plain=input.nextLine();
-
+        
                 String encrypted = encrypt(encrypt, plain);
                 System.out.println("Encrypted text: " + encrypted);
-
+        
                 String decrypted = decrypt(decrypt, encrypted);
                 System.out.println("Decrypted text: " + decrypted);
-
-                //file
-                System.out.print("File to read: ");
-                String path = input.nextLine();
-                System.out.print("File for encryption: ");
-                String fileenc = input.nextLine();
-                fileencrypt(desCipherEncrypter,path,fileenc);
-                System.out.print("File for decryption: ");
-                String filedec = input.nextLine();
-                filedecrypt(desCipherDecrypter, fileenc,filedec );
-               
-     
-            }
-                catch (NoSuchAlgorithmException noSuchAlgorithmException) {
-                noSuchAlgorithmException.printStackTrace();
-              } catch (InvalidKeyException invalidKeyException) {
-                invalidKeyException.printStackTrace();
-              } catch (NoSuchPaddingException noSuchPaddingException) {
-                noSuchPaddingException.printStackTrace();
-              } catch (BadPaddingException badPaddingException) {
-                badPaddingException.printStackTrace();
-              } catch (IllegalBlockSizeException illegalBlockSizeException) {
-                illegalBlockSizeException.printStackTrace();
-              } catch (FileNotFoundException e) {
-                e.printStackTrace();
-              } catch (Exception e) {
-                e.printStackTrace();
-              }
-     
-        }
-     
+                input.close();
+            
+          }
 
           //----------------------------------------TEXT ENCRYPTION/DECRYPTION----------------------------------------------//
           public static String encrypt(Cipher cipher, String plainStr) throws BadPaddingException,IllegalBlockSizeException
@@ -90,39 +78,22 @@ public class DES {
               byte[] decryptedBytes=cipher.doFinal(encryptedBytes);
               return new String(decryptedBytes,StandardCharsets.UTF_8);
           }
-     
-        public static void fileencrypt(Cipher cipher,String path,String encryptedfile) throws FileNotFoundException,Exception{
-            Scanner inputi=new Scanner(System.in);
-            Scanner finput = null;
-            File file = new File(path);
-            if (!file.exists()) throw new FileNotFoundException();
-            if (!file.canRead()) throw new Exception("Cannot read " + path);
-            if (!file.canWrite()) throw new Exception("Cannot write " + path);
-            finput = new Scanner(file);
-            FileWriter write =new FileWriter(encryptedfile);
-            while (finput.hasNext()) {
-                String str = finput.nextLine();
-                String encrypt = encrypt(cipher, str);
-                write.write(encrypt);
-                write.write("\n");
-            }
-            System.out.println("Encrypted text file");
-            write.close();
-        }
-     
-            public static void filedecrypt(Cipher cipher,String filetodecrypt,String dencryptedfile) throws FileNotFoundException,Exception{
-            Scanner dinput=null;
-            FileWriter write1 =new FileWriter(dencryptedfile);
-            File filefordecrypt=new File(filetodecrypt);
-            dinput=new Scanner(filetodecrypt);
-            while (dinput.hasNext()) {
-            String str = dinput.nextLine();
-            String decrypt = decrypt(cipher, str);
-            write1.write(decrypt + " ");
-        }
-            write1.close();
-            System.out.println("Decrypted text");
-    }
+           //----------------------------------------FILE ENCRYPTION/DECRYPTION----------------------------------------------//
+          private static void encryption(InputStream input, OutputStream output)
+          throws IOException
+          {
+              output = new CipherOutputStream(output, encrypt);
+              //calling the writeBytes() method to write the encrypted bytes to the file
+              writeBytes(input, output);
+          }
+          //method for decryption
+          private static void decryption(InputStream input, OutputStream output)
+                  throws IOException
+          {
+              input = new CipherInputStream(input, decrypt);
+              //calling the writeBytes() method to write the decrypted bytes to the file
+              writeBytes(input, output);
+          }
             
 
 
